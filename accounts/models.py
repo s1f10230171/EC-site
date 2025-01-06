644097c608a3debfc3267c70dbe0e7d87e69e3c1
1F10230171 <s1f102301718@iniad.org>
2025-01-06 23:49:31 +0900

    プロフィール画面の作成

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
