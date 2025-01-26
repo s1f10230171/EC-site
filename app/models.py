0783f90f452d291f87fcaa94c8ffa61e485d8ff6
1F10230171 <s1f102301718@iniad.org>
2025-01-26 15:52:38 +0900

    カート画面(削除+-表示なし)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Item(models.Model):
@@ -11,3 +12,32 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+    
+    def __str__(self):
+        return f'{self.item.title}:{self.quantity}'
+    
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_total_item_price()
+        return total
+    
+    def __str__(self):
+        return self.user.email
--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 urlpatterns=[
     path('', views.IndexView.as_view(), name='index'),
     path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
+    path('additem/<slug>', views.addItem, name='additem'),
+    path('order/', views.OrderView.as_view() , name='order'),
 ]
