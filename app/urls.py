293ae352903cb9a01b180e0b9edbcb3e133a4d37
1F10230171 <s1f102301718@iniad.org>
2025-01-29 17:26:35 +0900

    商品削除機能・減量・購入機能の追加

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,9 @@ urlpatterns=[
     path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
     path('additem/<slug>', views.addItem, name='additem'),
     path('order/', views.OrderView.as_view() , name='order'),
+    path('removeitem/<slug>', views.removeItem, name='removeitem'),
+    path('removesingle/<slug>', views.removeSingleItem, name='removesingleitem'),
+    path('payment/', views.PaymentView.as_view(), name='payment'),
+    path('thanks/', views.ThanksView.as_view(), name='thanks'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, Payment
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.models import CustomUser
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -53,3 +54,81 @@ class OrderView(LoginRequiredMixin, View):
             return render(request, 'app/order.html', context)
         except ObjectDoesNotExist:
             return render(request, 'app/order.html')
+        
+@login_required
+def removeItem(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order.exists():
+        order = order[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item, 
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            return redirect('order')
+        
+    return redirect('product', slug=slug)
+
+@login_required
+def removeSingleItem(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order = Order.objects.filter(
+        user=request.user, 
+        ordered=False
+    )
+    if order.exists():
+        order = order[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item = item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+                order_item.delete()
+            return redirect('order')
+    return redirect('product', slug=slug)
+
+class PaymentView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        order = Order.objects.get(user=request.user, ordered=False)
+        user_data = CustomUser.objects.get(id=request.user.id)
+        context = {
+            'order': order,
+            'user_data': user_data
+        }
+        return render(request, 'app/payment.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        order = Order.objects.get(user=request.user, ordered=False)
+        order_items = order.items.all()
+        amount = order.get_total()
+
+        payment= Payment(user=request.user)
+        payment.stripe_change_id = 'test_stripe_change_id'
+        payment.amount = amount
+        payment.save()
+
+        order_items.update(ordered=True)
+        for item in order_items:
+            item.save()
+
+        order.ordered = True
+        order.payment = payment
+        order.save()
+        return redirect('thanks')
+    
+class ThanksView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/thanks.html')
